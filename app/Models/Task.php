c5009df20cbb3fd736cac5161612555fabe0e365
haniful360 <haniful360@gmail.com>
2025-01-08 11:38:09 +0600

    add task database schema

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-
-
-    protected $fillable = ['name', 'description', 'status', 'due_date', 'user_id'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
+    //
 }
